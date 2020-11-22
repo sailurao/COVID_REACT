d52c8b52563e6cb936b40257c898d751f5d066a9
sailurao <nagar@teampdi.com>
2020-11-21 21:23:33 -0500

    Covid React project

---
+++ test/src/service/ApiService.js
@@ -0,0 +1,86 @@
+import axios from 'axios';
+
+const USER_API_BASE_URL = 'http://localhost:8080/users';
+const EMPLOYEE_API_BASE_URL = 'http://localhost:8080/employees';
+const EMPLOYEETR_API_BASE_URL = 'http://localhost:8080/employee-trs';
+
+const EMPLOYEE1_API_BASE_URL = 'http://localhost:8080/245678342/ighklsd'; //used to fetch single employee record by user id
+
+
+class ApiService {
+
+	//******************** users table *****************
+    fetchUsers() {
+        return axios.get(USER_API_BASE_URL);
+    }
+
+    fetchUserById(userId) {
+        return axios.get(USER_API_BASE_URL + '/' + userId);
+    }
+
+    deleteUser(userId) {
+        return axios.delete(USER_API_BASE_URL + '/' + userId);
+    }
+
+    addUser(user) {
+        return axios.post(""+USER_API_BASE_URL, user);
+    }
+
+    editUser(user) {
+        return axios.put(USER_API_BASE_URL + '/' + user.id, user);
+    }
+//******************************************************
+		
+		
+//******************** employees table *****************
+    fetchEmployees() {
+        return axios.get(EMPLOYEE_API_BASE_URL);
+    }
+
+    fetchEmployee(empId) {
+        return axios.get(EMPLOYEE1_API_BASE_URL + '/' + empId);
+    }
+
+    fetchEmployeeById(empId) {
+        return axios.get(EMPLOYEE_API_BASE_URL + '/' + empId);
+    }
+
+    deleteEmployee(empId) {
+        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + empId);
+    }
+
+    addEmployee(emp) {
+        return axios.post(""+EMPLOYEE_API_BASE_URL, emp);
+    }
+
+    editEmployee(emp) {
+        return axios.put(EMPLOYEE_API_BASE_URL + '/' + emp.emp_id, emp);
+    }
+//******************************************************
+		
+//******************** employees Transaction table *****************
+    fetchEmployeeTrs() {
+        return axios.get(EMPLOYEETR_API_BASE_URL);
+    }
+
+    fetchEmployeeTrById(empId) {
+        return axios.get(EMPLOYEETR_API_BASE_URL + '/' + empId);
+    }
+
+    deleteEmployeeTr(empId) {
+        return axios.delete(EMPLOYEETR_API_BASE_URL + '/' + empId);
+    }
+
+    addEmployeeTr(emp) {
+        return axios.post(""+EMPLOYEETR_API_BASE_URL, emp);
+    }
+
+    editEmployeeTr(emp) {
+        return axios.put(EMPLOYEETR_API_BASE_URL + '/' + emp.emp_id, emp);
+    }
+//******************************************************
+		
+		
+}
+
+export default new ApiService();
